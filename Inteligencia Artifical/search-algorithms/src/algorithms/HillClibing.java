ada6caa8c7a16dea4e4fa427e177c95dd2152a7a
Lucas Antonio <lucasantonio2063@gmail.com>
2023-04-10 19:00:36 -0300

    Finished Hill Clibing Algorithms

--- Inteligencia Artifical/search-algorithms/src/algorithms/HillClibing.java
+++ Inteligencia Artifical/search-algorithms/src/algorithms/HillClibing.java
@@ -2,10 +2,41 @@ package algorithms;
 
 import entities.Node;
 
+import java.util.Random;
+
 public class HillClibing{
 
+    private Node<Integer> first;
+    private static int[] values;
+
+    public HillClibing() {
+        generateRandomValues();
+    }
+
+    public HillClibing(int[] values) {
+        this.values = values;
+    }
+
+    private void generateRandomValues() {
+        this.values = new int[15];
+        for (int i = 0; i < values.length; i++) {
+            int n = new Random().nextInt((i+2) * 6);
+            if (n > 10) n = n / 3;
+            values[i] = n;
+        }
+    }
+
+    public void run() {
+        this.first= new Node<>(values[0]);
+        Node<Integer> actual = first;
+
+        for (int i = 1; i < values.length; i++) {
+            actual.setNeighbor(new Node<>(values[i]));
+            actual = actual.getNeighbor();
+        }
+    }
+
     public Integer maxGlobal(Node<Integer> first) {
-        print(first);
         Node<Integer> actual = first;
         Node<Integer> maxGlobal = first;
         while (actual.getNeighbor() != null) {
@@ -17,16 +48,40 @@ public class HillClibing{
             }
             actual = neighbor;
         }
+        if (maxGlobal.getNeighbor() == null) return maxGlobal.getValue();
         return first.getValue();
     }
 
-    private void print(Node<Integer> first) {
-        Node<Integer> actual = first;
-        while (actual != null){
-            System.out.print(actual.getValue()+" ");
-            actual = actual.getNeighbor();
+    @Override
+    public String toString() {
+        int maxValue = getMaxValue();
+
+        for (int i = maxValue; i > 0; i--) {
+            System.out.print("|");
+            for (int value : values) {
+                if (value == i) {
+                    System.out.print(i+"    ");
+                } else {
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < values.length; i++) {
+            System.out.print("----");
         }
         System.out.println();
+
+        return "Global maximum = " + new HillClibing().maxGlobal(first);
     }
 
+    private static int getMaxValue() {
+        int maxValue = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxValue = values[i];
+            }
+        }
+        return maxValue;
+    }
 }
--- Inteligencia Artifical/search-algorithms/src/application/Program.java
+++ Inteligencia Artifical/search-algorithms/src/application/Program.java
@@ -3,21 +3,15 @@ package application;
 import algorithms.HillClibing;
 import entities.Node;
 
-import java.util.LinkedList;
 import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
 
-        Node<Integer> first = new Node<>(0);
-        Node<Integer> actual = first;
+        // Hill Climb
 
-        for (int i = 1; i < 10; i++) {
-            int next = new Random().nextInt(i * 20 + 6);
-            actual.setNeighbor(new Node<>(next));
-            actual = actual.getNeighbor();
-        }
-
-        System.out.println("Global maximum = " + new HillClibing().maxGlobal(first));
+        HillClibing hillClibing = new HillClibing();
+        hillClibing.run();
+        System.out.println(hillClibing);
     }
 }
